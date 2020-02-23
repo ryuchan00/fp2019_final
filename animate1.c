61d9164632bf9f21dd52a8cf686bbaf67aa110aa
ryuchun00 <leokun0210@gmail.com>
2020-02-23 11:54:12 +0900

    コードを整理した

--- animate1.c
+++ animate1.c
@@ -9,16 +9,17 @@ int main(void) {
     struct color c2 = {255, 0, 0};
     int i;
     int j;
-    for (i = 0; i < 40; ++i) {
+    int sheets = 60;
+    for (i = 0; i < sheets; ++i) {
         // 三角形の描画に必要な計算式
-        double sx = sin(2 * PI * i / 40);
-        double cx = cos(2 * PI * i / 40);
+        double sx = sin(2 * PI * i / sheets);
+        double cx = cos(2 * PI * i / sheets);
         double rad = 20 - 10 * sx;
         double x = 150 + 100 * cx;
         double y = 100 + 50 * sx;
         // 四角形の描画に必要な計算式
-        double sx2 = sin(2 * PI * (i + 20) / 40);
-        double cx2 = cos(2 * PI * (i + 20) / 40);
+        double sx2 = sin(2 * PI * (i + sheets / 2) / sheets);
+        double cx2 = cos(2 * PI * (i + sheets / 2) / sheets);
         double rad2 = 20 - 10 * cx2;
         double x2 = 150 + 100 * cx2;
         double y2 = 100 + 50 * sx2;
