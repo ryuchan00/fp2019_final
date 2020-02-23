17daf89bdb4e7c5139a34b2acaf1638196c77840
ryuchun00 <leokun0210@gmail.com>
2020-02-23 11:42:16 +0900

    プログラムファイルを整理した

--- animate1.c
+++ animate1.c
@@ -1,35 +1,37 @@
 // goround --- go round in 3D space
 #include "img.h"
 #include <math.h>
+
 #define PI 3.14159265358979
 
 int main(void) {
-  struct color c1 = { 30, 255, 0 };
-  struct color c2 = { 255, 0, 0 };
-  int i;
-  int j;
-  for(i = 0; i < 40; ++i) {
-    double sx = sin(2*PI*i/40);
-    double cx = cos(2*PI*i/40);
-    double rad = 20 - 10*sx;
-    double x = 150 + 100*cx;
-    double y = 100 + 50*sx;
-    double sx2 = sin(2*PI*(i+20)/40);
-    double cx2 = cos(2*PI*(i+20)/40);
-    double rad2 = 20 - 10*cx2;
-    double x2 = 150 + 100*cx2;
-    double y2 = 100 + 50*sx2;
-    c2.r = 255 - j;
-    c2.g =  100 + j;
-    j += 2;
+    struct color c1 = {30, 255, 0};
+    struct color c2 = {255, 0, 0};
+    int i;
+    int j;
+    for (i = 0; i < 40; ++i) {
+        // 三角形の描画に必要な計算式
+        double sx = sin(2 * PI * i / 40);
+        double cx = cos(2 * PI * i / 40);
+        double rad = 20 - 10 * sx;
+        double x = 150 + 100 * cx;
+        double y = 100 + 50 * sx;
+        // 四角形の描画に必要な計算式
+        double sx2 = sin(2 * PI * (i + 20) / 40);
+        double cx2 = cos(2 * PI * (i + 20) / 40);
+        double rad2 = 20 - 10 * cx2;
+        double x2 = 150 + 100 * cx2;
+        double y2 = 100 + 50 * sx2;
+        // 色の変更
+        c2.r = 255 - j;
+        c2.g = 100 + j;
+        j += 2;
 
-    img_clear();
-    // img_fillcircle(c2, x, y, rad);
-    img_fillrect(c2, x2, y2, rad2, rad2);
-    double xx[] = {100,20,30,10};
-    double yy[] = {100,20,30,10};
-    // filltriangle(200, 100, 300, 100, 250, 200, 200, 100, 250, 0.5)
-    img_filltriangle(c2, x - rad, y, x + rad, y, x, y + rad);
-    img_write();
-  }
+        img_clear();
+        // 三角形
+        img_filltriangle(c2, x - rad, y, x + rad, y, x, y + rad);
+        // 四角形
+        img_fillrect(c2, x2, y2, rad2, rad2);
+        img_write();
+    }
 }
