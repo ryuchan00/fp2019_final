a63fe910e18fa69d72dd6f1d68cbfd4bc49bd0d0
ryuchun00 <leokun0210@gmail.com>
2020-02-23 12:31:12 +0900

    軌道上に楕円を配置した

--- animate1.c
+++ animate1.c
@@ -9,6 +9,8 @@ int main(void) {
     struct color c2 = {255, 0, 0};
     int i;
     int j;
+    int k;
+    int l = 0;
     int sheets = 60;
     for (i = 0; i < sheets; ++i) {
         // 上三角形の描画に必要な計算式
@@ -29,18 +31,35 @@ int main(void) {
         double rad3 = 20 - 10 * sx3;
         double x3 = 150 + 100 * cx3;
         double y3 = 100 + 50 * sx3;
+        // 軌道上に円を配置する
+
         // 色の変更
         c2.r = 255 - j;
         c2.g = 100 + j;
         j += 2;
 
         img_clear();
+
+        // 軌道上に円を配置する
+        if (l >= 3) { l = 0; }
+        for (k = 0; k < sheets; ++k) {
+            if ((k % 3) == l) {
+                double sx = sin(2 * PI * k / sheets);
+                double cx = cos(2 * PI * k / sheets);
+                double x = 150 + 100 * cx;
+                double y = 100 + 50 * sx;
+                img_fillcircle(c1, x, y, 2);
+            }
+        }
+        l++;
+
         // 上三角形
+        // 三角形
         img_filltriangle(c2, x - rad, y, x + rad, y, x, y + rad);
         // 四角形
         img_fillrect(c2, x2, y2, rad2, rad2);
         //下三角形
-        img_filltriangle(c2, x3 - rad3, y3, x +  rad3, y3, x3, y3 - rad3); 
+//        img_filltriangle(c2, x3 - rad3, y3, x + rad3, y3, x3, y3 - rad3);
         img_write();
     }
 }
